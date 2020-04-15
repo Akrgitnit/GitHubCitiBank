7959d070914dd5a6eed1cb1dd5a2c5c055cb6edc
Dev2 <Dev2local@gmail.com>
2020-04-15 19:54:23 +0530

    initial update by Dev2

--- GitTestProject/src/com/nt/test/Test.java
+++ GitTestProject/src/com/nt/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hii i am lucky");
         System.out.println("Hello lol");
+        System.out.println("I am a student");
 	}
 
 }
